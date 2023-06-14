b1e74d9eb9fc25ff59622849a3fd5be3a5c2f37a
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2023-06-14 16:25:22 +0900

    deviseエラーメッセージの実装、日本語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,14 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :avatar])
+  end
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+  def after_sign_in_path_for(resource_or_scope)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
   end
 end
