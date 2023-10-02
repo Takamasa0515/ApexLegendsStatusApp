0837fb8f60ccdad01f2a782469306ede2c5ae1e1
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2023-10-02 11:49:48 +0900

    Add: Userの新規作成,ログイン,ログアウト,プロフィール編集についてのシステムスペックを実装 #19”

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name { "test" }
-    email { "test@example.com" }
-    password { "testpassword" }
-    self_introduction { "テストデータです" }
+    name { "user" }
+    email { "user@example.com" }
+    password { "userpassword" }
+    self_introduction { "ユーザーです" }
   end
 
   factory :guest_user, class: "User" do
@@ -13,4 +13,10 @@ FactoryBot.define do
     self_introduction { "ゲストユーザーです" }
   end
 
+  factory :registered_user, class: "User" do
+    name { "登録済みユーザー" }
+    email { "registered_user@example.com" }
+    password { "registered_user_password" }
+    self_introduction { "ユーザーです" }
+  end
 end
