b3fa5682b8097fac7a9a42e18959dfd96d502f68
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2023-10-06 10:12:30 +0900

    Add: TrackerMatchRecordの試合履歴表示ページについてのシステムスペックを実装 #19

--- spec/factories/tracker_match_records.rb
+++ spec/factories/tracker_match_records.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tracker_match_record do
-    match_date { "2023-08-01 " }
+    match_date {  Date.today.beginning_of_month.to_s }
     legend { "Wraith" }
     kills { 10 }
     damages { 1800 }
