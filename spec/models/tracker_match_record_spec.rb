d0cb56ddbe936eccae345083f151344ace96e0d1
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2023-10-11 10:56:35 +0900

    Update: 試合日時を現在の月初になるよう変更 #19

--- spec/models/tracker_match_record_spec.rb
+++ spec/models/tracker_match_record_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe TrackerMatchRecord, type: :model do
     { "data" =>
       { "items" =>
         [
-          { "metadata" => { "endDate" => { "value" => "2023-08-01" } },
+          { "metadata" => { "endDate" => { "value" => Date.today.beginning_of_month.to_s } },
             "matches" => [ {
               "metadata" => {
-                "endDate"=>{ "value" => "2023-08-01" },
+                "endDate"=>{ "value" => Date.today.beginning_of_month.to_s },
                 "character"=>{ "displayValue" => "Wraith" }
               },
               "stats" => {
@@ -29,10 +29,10 @@ RSpec.describe TrackerMatchRecord, type: :model do
   let(:extract_day_history) do
     [
       {
-        "metadata" => { "endDate" => { "value" => "2023-08-01" } },
+        "metadata" => { "endDate" => { "value" => Date.today.beginning_of_month.to_s } },
         "matches" => [
           {
-            "metadata" => { "endDate" => { "value" => "2023-08-01" }, "character" => { "displayValue" => "Wraith" } },
+            "metadata" => { "endDate" => { "value" => Date.today.beginning_of_month.to_s }, "character" => { "displayValue" => "Wraith" } },
             "stats" => { "kills" => { "value" => 10 }, "damage" => { "value" => 1800 }, "wins" => { "value" => 1 } }
           }
         ]
@@ -43,7 +43,7 @@ RSpec.describe TrackerMatchRecord, type: :model do
   let(:one_match_history) do
     {
       "metadata" => {
-        "endDate" => { "value" => "2023-08-01" }, "character" => { "displayValue" => "Wraith" }
+        "endDate" => { "value" => Date.today.beginning_of_month.to_s }, "character" => { "displayValue" => "Wraith" }
       },
       "stats" => {
         "kills" => { "value" => 10 }, "damage" => { "value" => 1800 }, "wins" => { "value" => 1 }
@@ -54,7 +54,7 @@ RSpec.describe TrackerMatchRecord, type: :model do
   let(:no_value_one_match_history) do
         {
       "metadata" => {
-        "endDate" => { "value" => "2023-08-01" }, "character" => { "displayValue" => "Wraith" }
+        "endDate" => { "value" => Date.today.beginning_of_month.to_s }, "character" => { "displayValue" => "Wraith" }
       },
       "stats" => {
         "kills" => { "value" => 10 }, "damage" => { "value" => 1800 }
@@ -63,17 +63,18 @@ RSpec.describe TrackerMatchRecord, type: :model do
   end
 
   let(:extract_one_match_result) {
-    { "date" => "2023-08-01", "legend" => "Wraith", "kills" => 10, "damages" => 1800, "wins" => 1 }
+    { "date" => Date.today.beginning_of_month.to_s, "legend" => "Wraith", "kills" => 10, "damages" => 1800, "wins" => 1 }
   }
 
   let(:extract_match_result) {
-    [{"date" => "2023-08-01", "legend" => "Wraith", "kills" => 10, "damages" => 1800, "wins" =>1 }]
+    [{"date" => Date.today.beginning_of_month.to_s, "legend" => "Wraith", "kills" => 10, "damages" => 1800, "wins" =>1 }]
   }
 
   describe "API通信でデータを取得した時" do
     context "指定した日にデータがある場合" do
       it "全ての試合履歴が取得できている事" do
-        date = Date.new(2023, 8, 1)
+        today = Date.today
+        date = Date.new(today.year, today.month, 1)
         day_history = TrackerMatchRecord.fetch_day_match_history(match_history, date)
         expect(day_history).to eq extract_day_history
       end
