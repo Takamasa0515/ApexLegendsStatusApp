8100f0d08e9ace85f31d453aee37135344d7da9c
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2023-10-17 15:19:50 +0900

    Update: TrackerMatchRecordのApi通信についてのリクエストスペックを追加 #19

--- spec/requests/tracker_match_records_spec.rb
+++ spec/requests/tracker_match_records_spec.rb
@@ -3,6 +3,31 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   let(:user) { FactoryBot.create(:user) }
   let(:game_account_info) { FactoryBot.create(:game_account_info, user: user) }
+  let(:no_game_account_info) { FactoryBot.create(:no_game_account_info, user: user) }
+
+  describe "API通信を行う時" do
+    context "ゲームアカウントが存在する時" do
+      it "アカウント情報が返ってくること" do
+        result = TrackerMatchRecord.fetch_trn_match_history(game_account_info)
+        expect(result).to include("data")
+      end
+    end
+
+    context "ゲームアカウントが見つからない時" do
+      it "No accountを返す事" do
+        result = TrackerMatchRecord.fetch_trn_match_history(no_game_account_info)
+        expect(result).to eq "No account"
+      end
+    end
+
+    context "APIリクエスト数の上限に達した時" do
+      it "Apilimitを返す事" do
+        allow(TrackerMatchRecord).to receive(:fetch_trn_match_history).and_return("Apilimit")
+        result = TrackerMatchRecord.fetch_trn_match_history(game_account_info)
+        expect(result).to eq "Apilimit"
+      end
+    end
+  end
 
   describe "GET/user_tracker_match_records" do
     before do
