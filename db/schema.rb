1f1602d5bbe6a39f1177390cbcd5440889fe8ed7
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2023-11-09 11:12:09 +0900

    Remove:profileカラムを削除するマイグレーションファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_08_072958) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_03_023717) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_08_072958) do
     t.datetime "updated_at", null: false
     t.text "self_introduction"
     t.datetime "last_accessed_at_match_record"
+    t.string "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
