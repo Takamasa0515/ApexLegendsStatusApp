2dc053a1ab3a4d51a6368e8f59dd5608465a07f6
Takamasa0515 <mhe7x0i7l7e@icloud.com>
2023-11-24 09:13:05 +0900

    Add: ゲームアカウント変更時、変更前のアカウントの試合履歴を削除する機能を追加

--- app/controllers/game_account_info_controller.rb
+++ app/controllers/game_account_info_controller.rb
@@ -12,11 +12,18 @@ class GameAccountInfoController < ApplicationController
   def update
     @game_account_info = GameAccountInfo.find_or_initialize_by(user_id: current_user.id)
     if @game_account_info.update(game_account_info_params)
+      delete_match_record
       flash[:notice] = I18n.t('flash.update')
       redirect_to user_path
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def delete_match_record
+    user = @game_account_info.user
+    records = user.tracker_match_records.all
+    records.destroy_all
   end
 
   private
